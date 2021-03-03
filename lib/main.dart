b1adebbaca80e36d85e793e0413b1acb5c3a4a12
Matías Saavedra <m.saavedra06@ufromail.cl>
2021-03-03 01:56:50 -0300

    Creación de interfaz de editar item.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:proyectottm/src/pages/agregar_item.dart';
 import 'package:proyectottm/src/pages/login_page.dart';
+import 'package:proyectottm/src/pages/editar_item.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,10 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Main',
-      initialRoute: 'item',
+      initialRoute: 'editar',
       routes: {
         'login': (BuildContext context) => LoginPage(),
         'item': (BuildContext context) => AgregarItem(),
+        'editar': (BuildContext context) => EditarItem()
       },
     );
   }
